8625cf09002103852aba7ff9d042e3aa075e730c
Hristo Hristov <HHristov@infragistics.com>
2026-02-02 18:05:39 +0200

    add fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  // base: './',
   build: {
     chunkSizeWarningLimit: 10 * 1024 * 1024, // 10 MB
   },
